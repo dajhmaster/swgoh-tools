c14b5ee36f4909d848aa4755cd0c8396bb831c7b
Daniel Voyles <dman977@gmail.com>
2017-09-16 23:43:36 -0600

    Discord bot code
    Attempt 2 at correcting list of commands under '`help'

--- index.js
+++ index.js
@@ -21,21 +21,19 @@ client.on('message', message => {
 
 	if (message.content === '`help')
 	{
-		 message.channel.send("The List of the Commands!
-					\nPing Get your Ping... not
-					\ngetme Invite me!
-					\nversion I have a version?!?!
-					\nservers I am on servers!
-					\nlounge Come on my Lounge!
-					\nserverinfo Get the Info of your Server!
-					\navatar Get your Avatar!
-					\nroles Get the roles of this server!
-					\n
-					\n
-					\nSay Say Wiggle, fliping table or RIP")
+		 message.channel.send("The List of the Commands!")
 		}
 });
 
+//\nPing Get your Ping... not\ngetme Invite me!\nversion I have a version?!?!
+//					\nservers I am on servers!
+//					\nlounge Come on my Lounge!
+//					\nserverinfo Get the Info of your Server!
+//					\navatar Get your Avatar!
+//					\nroles Get the roles of this server!
+//					\n
+//					\n
+//					\nSay Say Wiggle, fliping table or RIP
 
 
 client.login(process.env.BOT_TOKEN);
